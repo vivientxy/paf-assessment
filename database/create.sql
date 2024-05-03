466b735a984ee5392bcedf3715cf1f36ba44f29a
vivientxy <vivientxy@hotmail.com>
2024-05-03 14:38:03 +0800

    .

--- database/create.sql
+++ database/create.sql
@@ -1,3 +1,13 @@
 -- Task 1
 -- Write SQL statements in this file to create the brewery database and 
 -- populate the database with the given SQL files
+
+DROP DATABASE IF EXISTS brewery;
+CREATE DATABASE brewery;
+USE brewery;
+
+SOURCE beers.sql;
+SOURCE breweries.sql;
+SOURCE categories.sql;
+SOURCE geocodes.sql;
+SOURCE styles.sql;
